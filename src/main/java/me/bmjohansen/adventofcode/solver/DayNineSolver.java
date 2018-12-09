255789756068edaa61485d9b1d7636d27f50e13a
Bendik Johansen <bendikj@gmail.com>
2018-12-09 08:23:43 +0100

    Solve second part of day 9

--- src/main/java/me/bmjohansen/adventofcode/solver/DayNineSolver.java
+++ src/main/java/me/bmjohansen/adventofcode/solver/DayNineSolver.java
@@ -2,57 +2,81 @@ package me.bmjohansen.adventofcode.solver;
 
 import java.util.ArrayList;
 
-public class DayNineSolver implements Solver<Integer, Integer> {
-    private static final int SPECIAL_VALUE = 23;
-    private static final int MARBLES_TO_LEFT = 7;
+public class DayNineSolver implements Solver<Long, Long> {
+    private static final long SPECIAL_VALUE = 23;
+    private static final long MARBLES_TO_LEFT = 7;
 
-    @Override
-    public Integer solveFirst(String input) {
-        final int players = Integer.parseInt(input.split(" ")[0]);
-        final int lastMarble = Integer.parseInt(input.split(" ")[6]);
-        final var playerScores = playMarbleGame(players, lastMarble);
+    private class Marble {
+        private final long value;
+        private Marble previous;
+        private Marble next;
 
-        return playerScores.stream().max(Integer::compareTo).orElse(-1);
+        public Marble(long value) {
+            this.value = value;
+            previous = this;
+            next = this;
+        }
     }
 
     @Override
-    public Integer solveSecond(String input) {
+    public Long solveFirst(String input) {
         final int players = Integer.parseInt(input.split(" ")[0]);
-        final int lastMarble = Integer.parseInt(input.split(" ")[6]);// * 100;
+        final long lastMarble = Long.parseLong(input.split(" ")[6]);
         final var playerScores = playMarbleGame(players, lastMarble);
 
-        return playerScores.stream().max(Integer::compareTo).orElse(0);
+        return playerScores.stream().max(Long::compareTo).orElse(0l);
     }
 
-    private ArrayList<Integer> playMarbleGame(int players, int lastMarble) {
-        final var circle = new ArrayList<Integer>();
-        circle.add(0);
-        final var playerScores = new ArrayList<Integer>(players);
-        for (var p = 0; p < players; p++) playerScores.add(0);
+    @Override
+    public Long solveSecond(String input) {
+        final int players = Integer.parseInt(input.split(" ")[0]);
+        final long lastMarble = Long.parseLong(input.split(" ")[6]) * 100;
+        final var playerScores = playMarbleGame(players, lastMarble);
+
+        return playerScores.stream().max(Long::compareTo).orElse(0l);
+    }
+
+    private ArrayList<Long> playMarbleGame(int players, long lastMarble) {
+        var current = new Marble(0);
+
+        final var scores = new ArrayList<Long>();
+        for (var p = 0; p < players; p++) scores.add(0l);
 
         var player = 0;
-        var position = 0;
 
         for (var marble = 1; marble <= lastMarble; marble++) {
             if (marble % SPECIAL_VALUE == 0) {
-                final var score = playerScores.get(player);
-                final var otherMarvel = position - MARBLES_TO_LEFT < 0 ?
-                        circle.size() + position - MARBLES_TO_LEFT :
-                        position - MARBLES_TO_LEFT;
+                final var oldScore = scores.get(player);
+                for (var i = 0; i < MARBLES_TO_LEFT; i++) {
+                    current = current.previous;
+                }
+                final var newScore = current.value + marble + oldScore;
 
-                final var newScore = score + marble + circle.get(otherMarvel);
-
-                playerScores.set(player, newScore);
-                circle.remove(otherMarvel);
-                position = otherMarvel;
+                scores.set(player, newScore);
+                current = remove(current);
             } else {
-                final var nextPos = (position + 1) % circle.size() + 1;
-                circle.add(nextPos, marble);
-
-                position = nextPos;
+                current = current.next;
+                current = insert(current, new Marble(marble));
             }
+
             player = (player + 1) % players;
         }
-        return playerScores;
+
+        return scores;
+    }
+
+    private Marble remove(Marble marble) {
+        marble.next.previous = marble.previous;
+        marble.previous.next = marble.next;
+        return marble.next;
+    }
+
+    private Marble insert(Marble current, Marble newNext) {
+        newNext.next = current.next;
+        current.next = newNext;
+        newNext.next.previous = newNext;
+        newNext.previous = current;
+
+        return newNext;
     }
 }
